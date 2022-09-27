6c5eaee45ae8d0fc64aa47cc37091557350763c3
rtnickv <69909475+rtnickv@users.noreply.github.com>
2022-09-27 16:07:59 -0400

    Add files via upload

---
+++ src/Main/Shooter/Shooter.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Basic/Mechanism.h>
+
+class Shooter: public Mechanism {
+    public:
+        int shooterPivotPosition;
+        int shooterBarrelPosition;
+        void pivotShooter();
+        void rotateBarrel();
+
+};
