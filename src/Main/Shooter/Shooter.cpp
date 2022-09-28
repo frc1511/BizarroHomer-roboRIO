c220545c9eceff9e0c121e594ef2303b3ab231fc
rtnickv <69909475+rtnickv@users.noreply.github.com>
2022-09-28 18:17:20 -0400

    Basic structure and variables added

--- src/Main/Shooter/Shooter.cpp
+++ src/Main/Shooter/Shooter.cpp
@@ -1,2 +1,16 @@
-#include <Basic/Shooter.h>
+#include <Shooter/Shooter.h>
 #include <Rev/CANSparkMax.h>
+#include <Wrappers/MotorController/CANMotorController.h>
+
+int shooterPivotPosition;
+int desiredShooterPivotPosition;
+int shooterBarrelPosition;
+int desiredShooterBarrelPosition;
+
+void Shooter::pivotShooter(void){
+    
+}
+
+void Shooter::rotateBarrel(void){
+
+}
--- src/Main/Shooter/Shooter.h
+++ src/Main/Shooter/Shooter.h
@@ -1,12 +1,21 @@
 #pragma once
 
 #include <Basic/Mechanism.h>
+#include <Hardware/HardwareManager.h>
+#include <Basic/Feedback.h>
 
 class Shooter: public Mechanism {
-    public:
-        int shooterPivotPosition;
-        int shooterBarrelPosition;
-        void pivotShooter();
-        void rotateBarrel();
+public:
+    int desiredShooterPivotPosition;
+    int desiredShooterBarrelPosition;
+    void pivotShooter();
+    void rotateBarrel();
+
+private:
+    int shooterPivotAngle;
+    int shooterBarrelPosition;
+    HardwareManager::ShooterRotationMotor rotateBarrel0 { CAN_SHOOTER_ROTATE_MOTOR };
+    HardwareManager::ShooterPivotMotor pivotBarrel0 { CAN_SHOOTER_PIVOT_MOTOR0 };
+    HardwareManager::ShooterPivotMotor pivotBarrel1 { CAN_SHOOTER_PIVOT_MOTOR1 };
 
 };
